5166ba6c4b6741e9e0a988d38daa7374f66f1128
Joseph Boscán <joeypc.py@gmail.com>
2020-01-02 16:59:58 -0400

    Agregando menu, parte 1

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
@@ -85,9 +86,13 @@ export class InicioPage implements OnInit {
     }
   ];
 
-  constructor() { }
+  constructor(private menuController: MenuController) { }
 
   ngOnInit() {
+  }
+
+  toggleMenu() {
+    this.menuController.toggle();
   }
 
 }
