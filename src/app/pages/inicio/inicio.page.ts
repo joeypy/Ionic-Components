7567d8ba32e06157676e586940f940ebfae85d2a
Joseph Boscán <joeypc.py@gmail.com>
2020-01-05 10:39:12 -0400

    Agregando cierre de la app con botón de back nativo

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
+import { MenuController, Platform } from '@ionic/angular';
 import { Observable } from 'rxjs';
 
 import { Componente } from '../../components/interfaces/interfaces';
@@ -10,15 +10,25 @@ import { DataService } from '../../services/data.service';
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
-export class InicioPage implements OnInit {
+export class InicioPage implements OnInit, OnDestroy, AfterViewInit {
 
   componentes: Observable<Componente[]>;
+  backButtonSubscription;
 
-  constructor(private menuController: MenuController,
+  constructor(private platform: Platform,
+              private menuController: MenuController,
               private dataService: DataService) { }
 
   ngOnInit() {
     this.componentes = this.dataService.getMenuOpts();
+  }
+  ngAfterViewInit() {
+    this.backButtonSubscription = this.platform.backButton.subscribe(() => {
+      navigator['app'].exitApp();
+    });
+  }
+  ngOnDestroy() {
+    this.backButtonSubscription.unsubscribe();
   }
 
   toggleMenu() {
